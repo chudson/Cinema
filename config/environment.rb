059a96de9d82175fe96b8440d0118a784be66405
michal <mludy@wp.pl>
2009-10-05 23:53:41 +0200

    test

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,9 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
+require 'acts_as_ferret'
+ActsAsFerret.index_dir = "#{RAILS_ROOT}/tmp/index"
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
